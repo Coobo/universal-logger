36fb1c12f9b77d823c94c1c8cdd6a904b06bc4a7
VIMGAS <victormoraesgs@hotmail.com>
2019-02-11 18:07:38 -0200

    testing on every environment

---
+++ test/development.test.js
@@ -0,0 +1,23 @@
+process.env.APP_NAME = '!@#$%¨&*()_+=-´`[]~^}{/?°ºª;:.,<>¹²³£¢¬\\|"\'';
+process.env.NODE_ENV = 'development';
+
+console.log(`\n\n\nStarting test on ${process.env.NODE_ENV} environment`);
+
+const logger = require('./../');
+
+console.log('Logger env:', logger.env, 'should be', process.env.NODE_ENV);
+
+console.log('Available Methods:', logger.availableLevels, '\n\n');
+const { app, api, sql } = logger;
+
+for (let log of [app, sql, api]) {
+  for (let level in log.levels) {
+    try {
+      log[level](`Trying to log on ${level} level`);
+    } catch (e) {
+      throw new Error(
+        `Failed attempting ${level} method on logger inside ${env} environment.`
+      );
+    }
+  }
+}
--- test/staging.test.js
+++ test/staging.test.js
@@ -1,22 +1,23 @@
 process.env.APP_NAME = '!@#$%¨&*()_+=-´`[]~^}{/?°ºª;:.,<>¹²³£¢¬\\|"\'';
-process.env.NODE_ENV = 'production';
+process.env.NODE_ENV = 'staging';
+
+console.log(`\n\n\nStarting test on ${process.env.NODE_ENV} environment`);
 
 const logger = require('./../');
 
-// console.log('env', logger.env);
-// console.log('config', logger.config);
-// console.log('version', logger.version);
-// console.log('available loggers', logger.availableLoggers);
+console.log('Logger env:', logger.env, 'should be', process.env.NODE_ENV);
+
 console.log('Available Methods:', logger.availableLevels, '\n\n');
 const { app, api, sql } = logger;
 
-console.log('Starting to test all levels on every log type');
 for (let log of [app, sql, api]) {
   for (let level in log.levels) {
     try {
       log[level](`Trying to log on ${level} level`);
     } catch (e) {
-      throw new Error(`Failed attempting ${level} method on logger`);
+      throw new Error(
+        `Failed attempting ${level} method on logger inside ${env} environment.`
+      );
     }
   }
 }
